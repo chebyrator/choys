21026a95977f8d41c9fca7bac0db437bfecee104
chebyrator <58294164+chebyrator@users.noreply.github.com>
2019-11-28 15:15:27 +0300

    Create copy.php

---
+++ copy.php
@@ -0,0 +1,8 @@
+<?php
+if (!defined('UNBANS'))
+{
+    exit ( "Попытка взлома? <a href=\"/\">Главная</a>." );
+}
+$version = "281119";
+?>
+<div align="center"><b>Разработчик:</b> <a href="https://pawno-crmp.ru/profile/developer" target="_blank"> Jaliba Baron</a><br><b> Версия панели:</b> <?php echo $version ?> <br>© PAWNO-CRMP.RU 2014-2020</div>  
