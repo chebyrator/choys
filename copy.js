1e8060b06551f4d2e9f9dfc36eec2d53cec49925
chebyrator <58294164+chebyrator@users.noreply.github.com>
2019-11-28 17:28:00 +0300

    Update copy.js

--- copy.js
+++ copy.js
@@ -1,1 +1,1 @@
-document.write("Разработчик: Jaliba Baron<br> Версия панели: 281119<br>© PAWNO-CRMP.RU 2014-2020");
+document.write("Разработчик: <a href="pawno-crmp.ru/profile/developer">Jaliba Baron</a><br> Версия панели: 281119<br>© PAWNO-CRMP.RU 2014-2020");
