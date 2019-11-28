c12f99db4d7b58ed6ff7d6650cda09020e5d5158
chebyrator <58294164+chebyrator@users.noreply.github.com>
2019-11-28 17:35:19 +0300

    Update copy.js

--- copy.js
+++ copy.js
@@ -1,1 +1,8 @@
-document.write("Разработчик: <a href="pawno-crmp.ru/profile/developer">Jaliba Baron</a><br> Версия панели: 281119<br>© PAWNO-CRMP.RU 2014-2020");
+<?php
+if (!defined('UNBANS'))
+{
+    exit ( "Попытка взлома? <a href=\"/\">Главная</a>." );
+}
+include_once 'core/configs.php';
+?>
+<div align="center"><b>Разработчик:</b> <a href="https://pawno-crmp.ru/profile/developer" target="_blank"> Jaliba Baron</a><br><b> Версия панели:</b> <?php echo $version ?> <br>© PAWNO-CRMP.RU 2014-2020</div>
