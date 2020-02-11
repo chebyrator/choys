c83b9cfd4b5bc2f27b589718796c858afd14b19e
chebyrator <58294164+chebyrator@users.noreply.github.com>
2020-02-11 14:50:46 +0300

    Update copy.php

--- copy.php
+++ copy.php
@@ -4,5 +4,5 @@ if (!defined('UNBANS'))
     exit ( "Попытка взлома? <a href=\"/\">Главная</a>." );
 }
 $version = "281119";
-echo '<b>Разработчик:</b> <a href="https://pawno-crmp.ru/profile/developer" target="_blank"> Jaliba Baron</a><br><b> Версия панели:</b> $version <br>© PAWNO-CRMP.RU 2014-2020';
+echo '<center><b>Разработчик:</b> <a href="https://pawno-crmp.ru/profile/developer" target="_blank"> Jaliba Baron</a><br><b> Версия панели:</b> $version <br>© PAWNO-CRMP.RU 2014-2020</center>';
 ?>
